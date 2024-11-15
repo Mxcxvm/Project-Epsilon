6cad3b0592bbcfa17daa310e5cb61be23ef3f802
Mxcxvm <Michael.ghebremussie@student.reutlingen-university.de>
2024-11-15 01:00:16 +0100

    Added Combat System
    Added Enemy, Added Death logic with kill animation

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -8,3 +8,8 @@ func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, 
 	
 func _on_timer_timeout():
 	get_tree().reload_current_scene()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "Destroyed":
+		queue_free()
