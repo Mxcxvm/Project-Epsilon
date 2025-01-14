aee263983e9108b0d90abe0f518f6c83ef0fd7b5
Alexander Klingel <Alexander.Klingel96@gmail.com>
2025-01-14 14:28:06 +0100

    new paralax, spear riddle

---
+++ scripts/spears_activator.gd
@@ -0,0 +1,35 @@
+extends Node2D
+@onready var animation_player: AnimationPlayer = $AnimationPlayer as AnimationPlayer
+#@export var toggle: bool = false
+#var state: bool = false
+# Called when the node enters the scene tree for the first time.
+var state: bool = false;
+signal spear_state(state)
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	#if (!doorOpened):
+		animation_player.play("activate")
+		state = true
+		spear_state.emit(state)
+	#else:
+		#if (state):
+			#animation_player.play("disable")
+		#else:
+			#animation_player.play("activate")
+#
+	#state = !state
+	
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	#if (!toggle):
+		animation_player.play("disable")
+		state = false
+		spear_state.emit(state)
