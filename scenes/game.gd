e45aa7d2485fe0f20392c94daca716509938a38c
Alexander Klingel <Alexander.Klingel96@gmail.com>
2025-02-03 03:06:27 +0100

    merge fix

--- scenes/game.gd
+++ scenes/game.gd
@@ -2,10 +2,8 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	# Clear the "players" group to remove any incorrectly added nodes
-	for node in get_tree().get_nodes_in_group("players"):
-		node.remove_from_group("players")
+func _ready() -> void:
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
